a13d66c2587104f40f2419469e73e879d6711a4e
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-08-18 19:09:25 -0300

    Obtendo valores

--- js/script.js
+++ js/script.js
@@ -5,5 +5,22 @@ const buttons = document.querySelectorAll("#buttons-container button")
 console.log(buttons)
 
 class Calculator {
-    
+    constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText
+    this.currentOperationText = currentOperationText
+    this.currentOperationText = ""
+    }
 }
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === ".") {
+            console.log(value);
+        } else {
+            console.log("Op: " + value);
+        }
+    });
+});
