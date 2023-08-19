f52895ac9d54df4df329c3adc9e98f9643d7b791
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-08-18 22:33:55 -0300

    Tratamentos de falhas
    Adicionado bloqueio para não utilizar mais de 1 "." e trocar o resultado do calculo pra cima do valor atual

--- js/script.js
+++ js/script.js
@@ -1,16 +1,63 @@
-const previousOperationText = document.querySelector("#previous-operation")
-const currentOperationText = document.querySelector("#current-operation")
-const buttons = document.querySelectorAll("#buttons-container button")
-
-console.log(buttons)
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
 
 class Calculator {
     constructor(previousOperationText, currentOperationText) {
-    this.previousOperationText = previousOperationText
-    this.currentOperationText = currentOperationText
-    this.currentOperationText = ""
+    this.previousOperationText = previousOperationText; //Sera exibido na tela
+    this.currentOperationText = currentOperationText; //Sera exibido na tela
+    this.currentOperation = ""; //Usuario ira digitar
     }
+    //add digit da tela da calculadora
+    addDigit(digit) {     
+        //Valida se ja existe um ponto, se houver não deixar utilizar outro
+        if(digit === "." && this.currentOperationText.innerText.includes(".")) {
+            return;
+        }
+
+        this.currentOperation = digit;
+        this.updateScreen();
+    }
+
+    //Todas as operações da calculadora
+    processOperation(operation) {
+        //Pega valor atual e anterior
+        let operationValue;
+        const previous = +this.previousOperationText.innerText;
+        const current = +this.currentOperationText.innerText;
+
+        switch(operation) {
+            case "+":
+                operationValue = previous + current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            default:
+                return;
+        }
+    }
+
+    //Mudando os valores na tela da calculadora
+    updateScreen(
+        operationValue = null, 
+        operation = null, 
+        current = null, 
+        previous = null
+    ) {
+        console.log(operationValue,operation,current,previous)
+
+        if(operationValue === null) {
+        this.currentOperationText.innerText += this.currentOperation;
+        } else {
+           //Checa se o valor é zero, se for apenas adicionar o valor atual
+           if(previous === 0) {
+            operationValue = current;
+           } 
+        }
+    }
+
 }
+
+const calc = new Calculator(previousOperationText, currentOperationText);
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -18,9 +65,9 @@ buttons.forEach((btn) => {
         const value = e.target.innerText;
 
         if(+value >= 0 || value === ".") {
-            console.log(value);
+            calc.addDigit(value);
         } else {
-            console.log("Op: " + value);
+            calc.processOperation(value);
         }
     });
 });
